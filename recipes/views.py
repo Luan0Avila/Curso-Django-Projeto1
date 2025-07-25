2b62cfe8b89452596580ad0ef8acf47c9b6b4465
Luan0AVila <avila.almeida.luan@gmail.com>
2025-07-25 13:09:05 -0300

    adicionando os testes

--- recipes/views.py
+++ recipes/views.py
@@ -2,15 +2,14 @@ from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': recipes
     })
 
 
@@ -21,7 +20,6 @@ def category(request, category_id):
             is_published=True,
         ).order_by('-id')
     )
-
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
@@ -30,7 +28,6 @@ def category(request, category_id):
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
-
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
@@ -42,7 +39,6 @@ def search(request):
 
     if not search_term:
         raise Http404()
-
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
