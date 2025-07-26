0c2d90d4c4dc70c9dd99ddde70161366a8987708
Luan0AVila <avila.almeida.luan@gmail.com>
2025-07-26 11:13:58 -0300

    fazendo a funcionalidade do search e realizando testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-
+from django.db.models import Q
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
@@ -8,6 +8,7 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
@@ -28,6 +29,7 @@ def category(request, category_id):
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
@@ -39,7 +41,16 @@ def search(request):
 
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+        Q(title__icontains = search_term) |
+        Q(description__icontains = search_term)
+        ), is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes
     })
