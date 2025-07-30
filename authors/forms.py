cd9edda37c2a0ee20a7e062afb5924fa02d6ff6d
Luan0AVila <avila.almeida.luan@gmail.com>
2025-07-30 11:53:03 -0300

    aprendendo maneirasde sobreescrever campos

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,41 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_placeholder(field, placeolder_val):
+    field.widget.attrs['placeholder'] = placeolder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your email')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+
+    password = forms.CharField(
+        required=True,
+        widget= forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one 1 uppercase letter'
+            'one lowercase letter and one number'
+            'at least 8 characters'
+        )
+        )
+    
+    password2 = forms.CharField(
+        required=True,
+        widget= forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+    )
+
     class Meta:
         model = User
         fields = ['first_name',
