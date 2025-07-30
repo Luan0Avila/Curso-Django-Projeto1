c50677cb762bbd9ca5d3516ee2feee8f55a8d6da
Luan0AVila <avila.almeida.luan@gmail.com>
2025-07-30 11:33:39 -0300

    aprendendo a usar labels

--- authors/forms.py
+++ authors/forms.py
@@ -8,4 +8,30 @@ class RegisterForm(forms.ModelForm):
                     'last_name',
                     'username', 
                     'email', 
-                    'password']
+                    'password'
+                ]
+        # exclue = ['first_name']
+        labels = {
+            'username': 'Username',
+            'first_name': 'Fist name',
+            'last_name': 'Last name',
+            'email': 'Email', 
+            'password': 'Password'
+        }
+        help_texts = {
+            'email': 'The email must be valid.',
+        }
+        error_messsages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+            'class': 'input text-input',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
