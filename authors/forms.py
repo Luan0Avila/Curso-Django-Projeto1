b58c6bd8a9b08822237e14a44ef61bc29b5f067e
Luan0AVila <avila.almeida.luan@gmail.com>
2025-08-07 12:45:15 -0300

    testando se os campos etão sendo requeridos

--- authors/forms.py
+++ authors/forms.py
@@ -33,6 +33,22 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Type your password')
         add_placeholder(self.fields['password2'], 'Repeat your password')
 
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text = 'The e-mail must be valid.',
+    )
+
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
@@ -49,7 +65,10 @@ class RegisterForm(forms.ModelForm):
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
-        label= 'Password2'
+        label= 'Password2',
+        error_messages={
+            'required':'Please, repeat your password',
+        }
     )
 
     class Meta:
@@ -64,13 +83,8 @@ class RegisterForm(forms.ModelForm):
         # exclude = ['first_name']
         labels = {
             'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
         }
-        help_texts = {
-            'email': 'The e-mail must be valid.',
-        }
+
         error_messages = {
             'username': {
                 'required': 'This field must not be empty',
--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -10,7 +10,7 @@ class AuthoRegisterFormUnitTest(TestCase):
     ('email', 'Your e-mail'),
     ('first_name', 'Ex.: John'),
     ('last_name', 'Ex.: Doe'),
-    ('pasword', 'Type your password'),
+    ('password', 'Type your password'),
     ('password2', 'Repeat your password'),
     ])
     def test_fields_placeholder(self, field, needed):
@@ -21,7 +21,7 @@ class AuthoRegisterFormUnitTest(TestCase):
     @parameterized.expand([
     ('username', 'Obrigatório. 150 caracteres ou menos. Letras, números e @/./+/-/_ apenas.'),
     ('email', 'The e-mail must be valid.'),
-    ('pasword', 'Password must have at least one uppercase letter, '
+    ('password', 'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'),
     ])
@@ -35,7 +35,6 @@ class AuthoRegisterFormUnitTest(TestCase):
         ('first_name', 'First name'),
         ('last_name', 'Last name'),
         ('email', 'E-mail'),
-        ('pasword', 'Password'),
         ('password2', 'Password2'),
         ])
     def test_fields_label(self, field, needed):
@@ -57,9 +56,16 @@ class AuthoRegisterFormIntegrationTest(DjangoTestCase):
     
     @parameterized.expand([
         ('username', 'This field must not be empty'),
+        ('first_name', 'Write your first name'),
+        ('last_name', 'Write your last name'),
+        ('password', 'Password must not be empty'),
+        ('password2', 'Please, repeat your password'),
+        ('email', 'E-mail is required'),
+        
     ])
     def test_fields_cannot_be_empty(self, field, msg):
         self.form_data[field] = ''
         url = reverse('authors:create')
         response = self.client.post(url, data=self.form_data, follow=True)
-        self.assertIn(msg, response.content.decode('utf-8'))
+        #self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get(field))
