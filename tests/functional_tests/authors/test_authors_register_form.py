b9164bb3fe9625cf7e4a2aca70e32fa3e5ae0dc8
Luan0AVila <avila.almeida.luan@gmail.com>
2025-08-25 13:01:19 -0300

    começando os testes no pagina de registro

--- tests/functional_tests/authors/test_authors_register_form.py
+++ tests/functional_tests/authors/test_authors_register_form.py
@@ -1,6 +1,10 @@
 from .base import AuthorsBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class AuthorsRegisterForm(AuthorsBaseTest):
         def test_the_test(self):
             self.browser.get(self.live_server_url + '/authors/register/')
+            form = self.browser.find_element(By.XPATH, '/html/body/main/div[2]/form')
+            first_name_field = form.find_element(By.XPATH, '//input[@placeholder="Ex.: John"]')
             self.sleep()
