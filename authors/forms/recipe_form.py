03d0b4b6bd27bdd21c2043bec92c52b4e5c9c73c
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-04 19:20:29 -0300

    alterando o css do formulario de edição de receita

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,7 +1,14 @@
 from django import forms
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 class AuhtorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields.get('title'), 'class', 'span-2')
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
+
     class Meta:
         model = Recipe
         fields = 'title', 'description', 'preparation_time','preparation_time_unit','servings','servings_unit','preparation_steps', 'cover'
