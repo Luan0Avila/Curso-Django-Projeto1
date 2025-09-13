26730db17b1ab820181d956f25b77bdadd42733f
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-13 19:04:27 -0300

    transportando o código da dashboard_recipe_edit para dentro da classe DashboardRecipe

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -7,18 +7,35 @@ from django.urls import reverse
 from django.contrib import messages
 
 class DashboardRecipe(View):
-    def get(self, request, id):
-        self.request
-        recipe = Recipe.objects.filter(is_published=False, author=request.user, pk=id,).first()
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.filter(is_published=False, author=self.request.user, pk=id,).first()
+        
         if not recipe:
             raise Http404
         
+        return recipe
+    
+    def render_recipe(self, form):
+        return render(self.request, 'authors/pages/dashboard_recipe.html',
+        context={'form': form})
+
+    def get(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+
+        return self.render_recipe(form)
+
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             request.POST or None,
             files=request.FILES or None,
             instance=recipe
-    )
-
+        )   
+        
         if form.is_valid():
             # agora o form é valido e eu posso tentar salvar
             recipe = form.save(commit=False)
@@ -31,7 +48,7 @@ class DashboardRecipe(View):
 
             messages.success(request, 'Your recipe was saved with sucess!')
             return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+        
+        return self.render_recipe(form)
 
-        return render(request, 'authors/pages/dashboard_recipe.html',
-        context={'recipe': recipe,
-                'form': form})
+
