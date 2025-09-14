4459dbc86e01e8ebe8fa8d9a9d7460a1e9d5962f
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-14 15:54:21 -0300

    criando a classe de Category

--- recipes/views.py
+++ recipes/views.py
@@ -28,8 +28,22 @@ class RecipeListViewBase(ListView):
         page_obj, pagination_range = make_pagination(
             self.request, ctx.get('recipes'), PER_PAGE)
         ctx.update({ 'recipes': page_obj, 'pagination_range': pagination_range })
-
         return ctx
+    
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(category__id= self.kwargs.get('category_id')
+        )
+        return qs
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
 
 def home(request):
     recipes = Recipe.objects.filter(
