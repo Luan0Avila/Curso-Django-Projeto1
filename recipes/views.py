9ce098047dab0d004e33e5f98a37513252484b21
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-14 16:02:31 -0300

    criando a classe para o search

--- recipes/views.py
+++ recipes/views.py
@@ -16,7 +16,6 @@ class RecipeListViewBase(ListView):
     context_object_name = 'recipes'
     paginate_by = None
     ordering = ['-id']
-    template_name = 'recipes/pages/home.html'
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
@@ -40,10 +39,36 @@ class RecipeListViewCategory(RecipeListViewBase):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(category__id= self.kwargs.get('category_id')
         )
+
         return qs
 
 class RecipeListViewSearch(RecipeListViewBase):
     template_name = 'recipes/pages/search.html'
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            Q(
+        Q(title__icontains = search_term) |
+        Q(description__icontains = search_term)
+        ),
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'additional_url_query': f'&q={search_term}'
+        })
+
+        return ctx
+
 
 def home(request):
     recipes = Recipe.objects.filter(
