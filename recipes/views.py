df83e716ee237cfef27fb5b782c7ac87302f08f8
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-14 15:44:26 -0300

    adicionando a home a classe ListView

--- recipes/views.py
+++ recipes/views.py
@@ -7,8 +7,29 @@ from django.core.paginator import Paginator
 from utils.pagination import make_pagination
 from django.contrib import messages
 import os
+from django.views.generic import ListView
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    paginate_by = None
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(is_published=True,)
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request, ctx.get('recipes'), PER_PAGE)
+        ctx.update({ 'recipes': page_obj, 'pagination_range': pagination_range })
+
+        return ctx
 
 def home(request):
     recipes = Recipe.objects.filter(
