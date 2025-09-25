dc48784c1d8a7688ac042c7e3eb7fedb4bd5e447
Luan0AVila <avila.almeida.luan@gmail.com>
2025-09-25 12:08:08 -0300

    finalizando commit anterior

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-
 from .models import Category, Recipe
-
+from tag.models import Tag
+from django.contrib.contenttypes.admin import GenericStackedInline
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
+class TagInline(GenericStackedInline):
+    model  = Tag
+    fields = 'name',
+    extra = 1
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -20,5 +24,8 @@ class RecipeAdmin(admin.ModelAdmin):
         "slug": ('title',)
     }
 
+    inlines = [
+        TagInline,
+    ]
 
 admin.site.register(Category, CategoryAdmin)
