9747ddb548a9cb568f89688782a4adeb56328322
Luan0AVila <avila.almeida.luan@gmail.com>
2025-09-26 13:03:16 -0300

    começando a separar o settings em arquivos diferentes

---
+++ project/settings/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from .environment import *
+from .installed_apps import *
+from .middlewares import *
+from .databases import *
+from .templates import *
+from .security import *
+from .i18n import *
+from .messages import *
+from .assets import *
