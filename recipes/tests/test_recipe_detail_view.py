bd5d7c439870875769d8c28b490ad33bb47cdb41
Luan0Avila <avila.almeida.luan@gmail.com>
2025-09-26 22:08:29 -0300

    fazendo a validação pelo models

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -10,7 +10,7 @@ class RecipeDetailViewsTest(RecipeTestBase):
         view = resolve(
             reverse('recipes:recipe', kwargs={'pk': 1})
         )
-        self.assertIs(view.func.view_class, views.RecipeDetailView)
+        self.assertIs(view.func.view_class, views.DetailView)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
