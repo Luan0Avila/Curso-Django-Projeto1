2d24392fdd085d2fe1c27392813b23a16e2b75d0
Luan0AVila <avila.almeida.luan@gmail.com>
2025-10-02 12:34:13 -0300

    traduzindo diretamente na view

--- recipes/views.py
+++ recipes/views.py
@@ -14,6 +14,7 @@ from django.db.models.aggregates import Count
 from django.db.models.functions import Concat
 from tag.models import Tag
 from django.utils import translation
+from django.utils.translation import gettext as _
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -60,9 +61,10 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+        category_translation = _('Category')
 
         ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category | '
+            'title': f'{ctx.get("recipes")[0].category.name} - {category_translation} | '
         })
 
         return ctx
