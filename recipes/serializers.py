233c31a227b61713215be3725c119e0d6b08245f
Luan0AVila <avila.almeida.luan@gmail.com>
2025-10-06 12:40:00 -0300

    criando um serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -4,3 +4,4 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    
