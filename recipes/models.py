4c2133da799901ac11dca717f6519b4fe521f544
Luan0AVila <avila.almeida.luan@gmail.com>
2025-10-08 12:48:57 -0300

    Campos relacionados

--- recipes/models.py
+++ recipes/models.py
@@ -28,7 +28,9 @@ class RecipeManagager(models.Manager):
         F('author__last_name'), Value(' ('), 
         F('author__username'), Value(')')
         )
-        ).order_by('-id')
+        ).order_by('-id')\
+            .select_related('category', 'author')\
+                .prefetch_related('tags')
 
 class Recipe(models.Model):
     objects = RecipeManagager()
