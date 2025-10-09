88b0feee0fb756056a5b212aa303ad32d97d35eb
Luan0AVila <avila.almeida.luan@gmail.com>
2025-10-09 12:49:56 -0300

    migrando para o modelserializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from tag.models import Tag
+from .models import Recipe
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id','name','slug']
+    #id = serializers.IntegerField()
+    #name = serializers.CharField(max_length=255)
+    #slug = serializers.SlugField()
 
-class RecipeSerializer(serializers.Serializer):
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id','title','description','category','author','tags','public','preparation',
+                'tag_objects','tag_links']
+
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField(method_name='get_preparation')  
+    public = serializers.BooleanField(source='is_published', read_only=True)
+    preparation = serializers.SerializerMethodField(method_name='get_preparation',read_only=True)  
     category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
-    tag_objects = TagSerializer(many=True, source='tags')
-    tag_links = serializers.HyperlinkedRelatedField(many=True, source='tags', queryset=Tag.objects.all(), view_name='recipes:recipes_api_v2_tag')
+    tag_objects = TagSerializer(many=True, source='tags', read_only=True)
+    tag_links = serializers.HyperlinkedRelatedField(many=True, source='tags', read_only=True, view_name='recipes:recipes_api_v2_tag')
 
 
 
